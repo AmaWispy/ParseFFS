a526ed753ba3d86488fcbfafd7a518a2e7315695
Wispy <nowispy@mail.ru>
2018-12-03 17:55:51 +0300

    test

--- ParserFSS/Form1.cs
+++ ParserFSS/Form1.cs
@@ -7,14 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenQA.Selenium;
 
 namespace ParserFSS
 {
     public partial class Form1 : Form
     {
+        public IWebDriver browser;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            browser.Manage().Window.Maximize();
+            browser.Navigate().GoToUrl("http://google.com");
+
+            IWebElement search = browser.FindElement(By.Name("q"));
+            search.SendKeys("как поднять ммр" + OpenQA.Selenium.Keys.Enter);
         }
     }
 }
